8c76e621c2faa190d05846e46fab2710fce755c0
Andy_Lee <120154249+jungdadak@users.noreply.github.com>
2024-10-20 16:14:57 +0900

    whyerr

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
-import express from "express";
-import taskApi from "./task.api.js";
+import express from 'express';
+import taskApi from './task.api.js';
+import userApi from './user.api.js';
 const router = express.Router();
 
-router.use("/tasks", taskApi);
+router.use('/tasks', taskApi);
+router.use('/user', userApi);
 
 export default router;
