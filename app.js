17ae48e9e04ff0a139e46e42dcfe03adac67f347
jungdadak <junghyun21110@gmail.com>
2024-10-20 23:36:15 +0900

    'dsfkljgldskjglsdrfkjglkdfkjglkd'

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import indexRouter from './routes/index.js';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import indexRouter from "./routes/index.js";
+import dotenv from "dotenv";
 dotenv.config();
 
 const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
 const app = express();
 app.use(cors());
 // JSON 요청 본문을 처리
 app.use(express.json());
 
 // 라우터 설정
-app.use('/api', indexRouter);
+app.use("/api", indexRouter);
 const mongoURI = MONGODB_URI_PROD;
 // MongoDB URI (환경 변수에서 불러오기)
 
 // MongoDB 연결
 mongoose
-  .connect(mongoURI)
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => {
-    console.log('DB connection failed', err);
-  });
+	.connect(mongoURI)
+	.then(() => console.log("MongoDB connected"))
+	.catch((err) => {
+		console.log("DB connection failed", err);
+	});
 
 // 서버 시작
 app.listen(PORT || 5001, () => {
-  console.log('Server listening on port ' + PORT);
+	console.log("Server listening on port " + PORT);
 });
