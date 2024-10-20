3b72ac59e0bb301924c03b4d07d414d5aab7b860
Andy_Lee <120154249+jungdadak@users.noreply.github.com>
2024-10-20 15:46:29 +0900

    portchange

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import indexRouter from "./routes/index.js";
-import dotenv from "dotenv";
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import indexRouter from './routes/index.js';
+import dotenv from 'dotenv';
 dotenv.config();
 
 const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
@@ -12,19 +12,19 @@ app.use(cors());
 app.use(express.json());
 
 // 라우터 설정
-app.use("/api", indexRouter);
+app.use('/api', indexRouter);
 const mongoURI = MONGODB_URI_PROD;
 // MongoDB URI (환경 변수에서 불러오기)
 
 // MongoDB 연결
 mongoose
-	.connect(mongoURI)
-	.then(() => console.log("MongoDB connected"))
-	.catch((err) => {
-		console.log("DB connection failed", err);
-	});
+  .connect(mongoURI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => {
+    console.log('DB connection failed', err);
+  });
 
 // 서버 시작
 app.listen(process.env.PORT || 5000, () => {
-	console.log("Server listening on port 5000");
+  console.log('Server listening on port ' + process.env.PORT);
 });
