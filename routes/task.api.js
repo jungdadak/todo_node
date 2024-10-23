845f32f1a3406c47798911dacd0d69cab603e69b
jungdadak <junghyun21110@gmail.com>
2024-10-23 22:57:28 +0900

    'smm'

--- routes/task.api.js
+++ routes/task.api.js
@@ -1,8 +1,9 @@
 import express from "express";
 import taskController from "../controller/task.controller.js";
+import authController from "../controller/auth.controller.js";
 const router = express.Router();
 
-router.post("/", taskController.createTask);
+router.post("/", authController.autehticate, taskController.createTask);
 router.get("/", taskController.getTask);
 
 router.put("/:id", taskController.updateTask);
